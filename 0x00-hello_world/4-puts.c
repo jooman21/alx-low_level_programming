9cec13160f3ae2b12ddae679d85ebba4ace507b1
jooman21 <jooman2121@gmail.com>
2022-03-10 11:37:54 -0800

    wqE

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,16 @@
-#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like buliding a multilingual puzzle");
+	return (0);
+}	
+
+
 
