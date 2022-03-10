1c779adb630b5bd6ea52593084924f9ec24591ea
jooman21 <jooman2121@gmail.com>
2022-03-10 14:55:04 -0800

    wooo

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,12 +17,11 @@ int main(void)
 {
             /* sizof evaluates the size of the variable */
 	    printf("Size of a char: %lu byte(s)\n", sizeof(char));
-	    printf("Size of an int: %lu bytes(s)\n",sizeof(int));
-	    printf("Size of a double: %lu bytes(s)\n",sizeof(double));
-	    printf("Size of a long int: %lu bytes(s)\n",sizeof(long int));
-	    printf("Size of a long long int: %lu bytes(s)\n",sizeof(long long int));
-	    printf("Size of a flot: %lu byte(s)\n",sizeof(float));
+	    printf("Size of an int: %lu bytes(s)\n", sizeof(int));
+	    printf("Size of a double: %lu bytes(s)\n", sizeof(double));
+	    printf("Size of a long int: %lu bytes(s)\n", sizeof(long int));
+	    printf("Size of a long long int: %lu bytes(s)\n", sizeof(long long int));
+	    printf("Size of a flot: %lu byte(s)\n", sizeof(float));
 
 	    return (0);
-
 }
