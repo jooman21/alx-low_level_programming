0530c81a13c6cce4a110c355109c1ffebe2dc45e
jooman21 <jooman2121@gmail.com>
2022-03-10 14:04:45 -0800

    wooo

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+
+
+/**
+ *
+ * main - Entry point
+ *
+ * Description: using sizeof to print the size of  various types.  
+ *
+ * Return: Always 0 (Success)
+ *
+ *     */
+
+int main(void)
+
+{
+            /* sizof evaluates the size of the variable */
+	    
+	    return (0);
+
+}
