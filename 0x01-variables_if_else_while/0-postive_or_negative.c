e14ef71a736a0f1d011113b2148ad4a605e723e5
jooman21 <jooman2121@gmail.com>
2022-03-11 06:55:30 -0800

    task0

--- 0x01-variables_if_else_while/0-postive_or_negative.c
+++ 0x01-variables_if_else_while/0-postive_or_negative.c
@@ -1,13 +1,13 @@
+#include <stdio.h>
+
 #include <stdlib.h>
 
 #include <time.h>
 
-/* more headers goes there */
-
-
-
-/* betty style doc for function main goes there */
-
+/*
+ * main Lanzador
+ * Return: Always 0
+ */  
 int main(void)
 
 {
