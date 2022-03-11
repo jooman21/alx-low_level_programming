0ee2e2217731b00c8321e77adef1bad51563aada
jooman21 <jooman2121@gmail.com>
2022-03-10 16:30:58 -0800

    wooo

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,13 +5,12 @@
 * Return: This time we return an error 1.
 */
 
-
-
 int main(void)
 
 {
 	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	write(1, quo, 59);
+
 	return (1);
 }
 	
