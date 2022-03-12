c3b84708e3d07f09b7171a2d0eca1cfb39631fdf
jooman21 <jooman2121@gmail.com>
2022-03-12 09:07:48 -0800

    qwe

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,19 +1,18 @@
 #include<stdio.h>
 
 /**
-*
 * main - Entry point
-*Description: print numbers of base16 in lowercase
-*Return: Always 0 (Success)
+* Description: print numbers of base16 in lowercase
+* Return: Always 0 (Success)
 */
-
-int main(void)
-
+int main(void
+		)
 {
 	int n = 48;
 	while (n <= 102)
         {				
 	        putchar(n);
+
 		if (n == 57)
 
 			n += 39;
@@ -23,5 +22,4 @@ int main(void)
 
 	putchar('\n');
 	return (0);
-
 }
