348092ec67b25f31bfcc202069e1d49c1cf177d0
jooman21 <jooman2121@gmail.com>
2022-03-12 07:18:33 -0800

    ad

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,26 @@
+#inlude<stdio.h>
+/**
+* main -Entry point 
+*
+* Description: print alphabet in lowercase than in uppercase
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char l = 'a';
+	char U = 'A';
+
+	while (l <= 'Z')
+	{
+		putchar(1);
+		++1;
+	}
+	while (U <= 'Z')
+	{
+		putchar(U);
+		++U;
+	}
+	putchar ('\n');
+	return (0);
+}	
