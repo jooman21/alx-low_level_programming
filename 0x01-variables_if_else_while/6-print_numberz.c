46221154000c90acdc9fcfbb1c127f03697f63f4
jooman21 <jooman2121@gmail.com>
2022-03-12 08:49:31 -0800

    qwe

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+/**
+* main - Entry point
+* Description: print 0-9 using putchar
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+
+{
+
+	int n = 0;
+	while (n <= 9)
+
+	{
+
+		putchar(n + '0');
+
+		++n;
+
+	}
+
+		putchar('\n');
+		return (0);
+
+}
