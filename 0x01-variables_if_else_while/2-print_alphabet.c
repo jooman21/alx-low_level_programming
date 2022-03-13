d2a6ba3d204fe34252761f553800b4e91f5c801a
jooman21 <jooman2121@gmail.com>
2022-03-13 05:49:21 -0700

    asd

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,16 +5,15 @@
 * Description: print all lowercase alphabets respectively
 * Return: Always 0 (Success)
 */
-
 int main(void)
 {
-	char ch = 'a';
-
-	while (ch <= 'z')
-	{
-		putchar(ch);
-		ch++;
-	}
-	putchar('\n');
-	return (0);
+char ch = 'a';
+while (ch <= 'z')
+{
+putchar(ch);
+ch++;
 }
+putchar('\n');
+return (0);
+}
+
