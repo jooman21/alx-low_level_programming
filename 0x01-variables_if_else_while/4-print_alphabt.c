142975bfba47a26a44b535701081b7beeb9184eb
jooman21 <jooman2121@gmail.com>
2022-03-13 06:48:19 -0700

    asd

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,5 +1,4 @@
 #include<stdio.h>
-
 /**
 * main - Entry point
 * Description: print all aplhabet letters except q and e
@@ -7,20 +6,16 @@
 */
 
 int main(void)
-
 {
-	char ch = 'a';
-	while (ch <= 'z')
-
-	{
-	       if (ch == 'e' || ch == 'q')
-	                ++ch;
-	       putchar(ch);
-
-			++ch;
-
-	}
-		putchar('\n');
-		return (0);
+char ch = 'a';
+while (ch <= 'z';
+if (ch == 'e' || ch == 'q')
+++ch;
+putchar(ch);
+++ch;
+}
+putchar('\n');
+return (0);
 
 }
+
