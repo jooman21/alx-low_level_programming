e222f692366481870ef81d0cd636db79fed3d465
jooman21 <jooman2121@gmail.com>
2022-03-13 14:15:27 -0700

    asd

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,47 +7,36 @@
 */
 
 int main(void)
-
 {
 
-	int n = 0;
-	int m, o;
-
-	while (n <= 9)
-
-        {
-
-		m = 0;
-
-		while (m <= 9)
-
-		{
-			o = 0;
-			while (o <= 9)
-
-           	        {
-				if (n != m && n < m && m != o && m < o)
-				{
-					putchar(n + 48);																											      putchar(m + 48);
-																			       putchar(o + 48);
-																			       if (n + m + o != 24)
-
-					{
-																			               putchar(',');
-						putchar(' ');
-																				}
-																			}
-				 ++o;																													}
-																		 ++m;
-																	}
-
-		 ++n;
-
-	}
-
-	putchar('\n')	
-	return (0);
-
+int n = 0;
+int m, o;
+while (n <= 9)
+{
+m = 0;
+while (m <= 9)
+{
+o = 0;
+while (o <= 9)
+{
+if (n != m && n < m && m != o && m < o)
+{
+putchar(n + 48);
+putchar(m + 48);
+putchar(o + 48);
+if (n + m + o != 24)
+{
+putchar(',');	
+putchar(' ');
 }
-
+}
+++o;
+}
+++m;
+}
+++n;
+}
+putchar('\n')	
+return (0);
+}
 
