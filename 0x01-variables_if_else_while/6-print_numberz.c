f4c3aec107df0c0cf0571b05ccb4d6c21c995132
jooman21 <jooman2121@gmail.com>
2022-03-13 12:58:15 -0700

    asd

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,27 +1,18 @@
 #include<stdio.h>
-
 /**
 * main - Entry point
 * Description: print 0-9 using putchar
 * Return: Always 0 (Success)
 */
-
 int main(void)
-
 {
-
-	int n = 0;
-	while (n <= 9)
-
-	{
-
-		putchar(n + '0');
-
-		++n;
-
-	}
-
-		putchar('\n');
-		return (0);
-
+int n = 0;
+while (n <= 9)
+{
+putchar(n + '0');
+++n;
 }
+putchar('\n');
+return (0);
+}
+
