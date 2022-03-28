10e15805bcef5c5c6c33ab86439e3ec1962b442e
jooman21 <yohannesgezahugne59@gmail.com>
2022-03-28 23:01:27 +0300

    pointers

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -15,8 +15,7 @@ unsigned int length;
 length = 0;
 for (i = 0; s[i] != '\0'; i++)
 {
-for (j = 0; accept[j] != '\0' && accept[j] != s[i]; j++)
-			;
+for (j = 0; accept[j] != '\0' && accept[j] != s[i]; j++);
 if (s[i] == accept[j])
 length++;
 if (accept[j] == '\0')
