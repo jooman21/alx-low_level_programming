93a79e3554391200871a6f9856a92e87cf668775
jooman21 <yohannesgezahugne59@gmail.com>
2022-03-30 10:10:53 +0300

    recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -8,17 +8,17 @@
  **/
 int sqtRecursive(int start, int end, int m)
 {
-long mid;
+	long mid;
 
-if (end >= start)
-{
+	if (end >= start)
+	{
 mid = start + (end - start) / 2;
-if (mid * mid == m)
+if (mid *mid == m)
 return (mid);
 /* following binary search */
-if (mid * mid > m)
+if (mid *mid > m)
 return (sqtRecursive(start, mid - 1, m));
-if (mid * mid < m)
+if (mid *mid < m)
 return (sqtRecursive(mid + 1, end, m));
 }
 return (-1);
