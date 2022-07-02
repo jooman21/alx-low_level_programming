2cec73e2b4e9b692e8ca1c6860b7b6dd51e5c3fc
jooman21 <yohannesgezahugne59@gmail.com>
2022-07-02 07:43:12 +0300

    joo21

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,30 +1,37 @@
 #include "hash_tables.h"
+#include <stdlib.h>
 
 /**
- * hash_table_delete - Deletes a hash table.
- * @ht: A pointer to a hash table.
+ * hash_chain_delete - delete a singly-linked list
+ * @head: a pointer to the head of the singly-linked list
+ */
+void hash_chain_delete(hash_node_t *head)
+{
+	if (head)
+	{
+		hash_chain_delete(head->next);
+		free(head->key);
+		free(head->value);
+		free(head);
+	}
+}
+
+/**
+ * hash_table_delete - delete a hash table
+ * @ht: a pointer to the hash table
  */
 void hash_table_delete(hash_table_t *ht)
 {
-	hash_table_t *head = ht;
-	hash_node_t *node, *tmp;
-	unsigned long int i;
+	hash_node_t **array = NULL;
+	unsigned long int index = 0;
+	unsigned long int size = 0;
 
-	for (i = 0; i < ht->size; i++)
+	if (ht)
 	{
-		if (ht->array[i] != NULL)
-		{
-			node = ht->array[i];
-			while (node != NULL)
-			{
-				tmp = node->next;
-				free(node->key);
-				free(node->value);
-				free(node);
-				node = tmp;
-			}
-		}
+		for (array = ht->array, size = ht->size; index < size; ++index)
+			hash_chain_delete(array[index]);
+
+		free(ht->array);
+		free(ht);
 	}
-	free(head->array);
-	free(head);
 }
